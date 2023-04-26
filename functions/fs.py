24ed3617c6146248e00cc424f1802b9d5da71562
ajcpereira <85030333+ajcpereira@users.noreply.github.com>
2023-04-26 20:08:08 +0100

    Update fs.py

--- functions/fs.py
+++ functions/fs.py
@@ -13,8 +13,8 @@ def fs(hostname, ssh, PLATFORM, PLATFORM_NAME, type, PLATFORM_REPO, PLATFORM_REP
                 logging.info("Starting metrics - %s" % log_stamp)
                 #CMD1="/opt/fsc/CentricStor/bin/rdNsdInfos -a > /tmp/stats_nsd.out"
                 #CMD2="iostat -x -k 1 2| awk '!/^sd/'|awk -vN=2 '/avg-cpu/{++n} n>=N' > /tmp/stats_iostat.out"
-                #CMD3="awk 'NR==FNR{a[$1]=$0; next} $3 in a{print a[$3],$0}' /tmp/stats_iostat.out /tmp/stats_nsd.out | awk '{print $18" "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9" "$10" "$11" "$12" "$13" "$14" "$15" "$16" "$17}' | sort"
-                CMD3="cat /tmp/stats_iostat.out"
+                CMD3="awk 'NR==FNR{a[$1]=$0; next} $3 in a{print a[$3],$0}' /tmp/stats_iostat.out /tmp/stats_nsd.out | awk '{print $18" "$1" "$2" "$3" "$4" "$5" "$6" "$7" "$8" "$9" "$10" "$11" "$12" "$13" "$14" "$15" "$16" "$17}' | sort > /tmp/cafs_iostat.fjcollect"
+                
                 if PLATFORM_USE_SUDO == "yes":
                     CMD1 = "sudo " + CMD1
                     CMD2 = "sudo " + CMD2
@@ -25,17 +25,12 @@ def fs(hostname, ssh, PLATFORM, PLATFORM_NAME, type, PLATFORM_REPO, PLATFORM_REP
                 stdin, stdout, stderr = ssh.exec_command(CMD3, get_pty=True)
                 timestamp = int(time.time())
 		
-                response = stdout.read().decode('ascii')
-                response2 = stdout.read().decode('ascii')
-                response3 = stdout.readlines()
+                response = stdout.read().decode('ascii').strip("\n")
                 
                 logging.info("SSH Output on function FS - %s" % response)
-                logging.info("SSH Output on function FS decode - %s" % response2)
-                logging.info("SSH Output on function FS readlines - %s" % response3)
              
                 for line in response.splitlines():
                     #if len(line.split())==16 and not line.startswith("\n") and not line.startswith("Device"):
-                    logging.info("SSH Output on function FS response - %s" % line)
                     columns = line.split()
                     logging.info("SSH Output on function FS response columns - %s" % columns)
                     netcat(PLATFORM_REPO, PLATFORM_REPO_PORT, PLATFORM_REPO_PROTOCOL,  str(PLATFORM) + "." + str(PLATFORM_NAME) + "." + str(type) + "." + str(columns[0]) + "." + str(columns[1]) + "." + str(columns[17]) + "." + "svctm" + " " + re.sub(",",".",columns[15]) +" "+ str(timestamp) + "\n")
