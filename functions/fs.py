8b5a19f0275e39ef950534c4693d37668a3cd805
ajcpereira <85030333+ajcpereira@users.noreply.github.com>
2023-04-26 19:19:49 +0100

    Update fs.py

--- functions/fs.py
+++ functions/fs.py
@@ -26,17 +26,21 @@ def fs(hostname, ssh, PLATFORM, PLATFORM_NAME, type, PLATFORM_REPO, PLATFORM_REP
                 timestamp = int(time.time())
 		
                 #response = stdout.read().decode('ascii')
+		
                 response = stdout.read()
+                response2 = stdout.read().decode('ascii')
                 
                 logging.info("SSH Output on function FS - %s" % response)
-                response2 = stdout.read().decode('ascii')
                 logging.info("SSH Output on function FS decode - %s" % response2)
              
                 for line in response.splitlines():
                     #if len(line.split())==16 and not line.startswith("\n") and not line.startswith("Device"):
-                    columns = line.split()			
+                    logging.info("SSH Output on function FS response - %s" % line)
+                    columns = line.split()
+                    logging.info("SSH Output on function FS response columns - %s" % columns)
                     netcat(PLATFORM_REPO, PLATFORM_REPO_PORT, PLATFORM_REPO_PROTOCOL,  str(PLATFORM) + "." + str(PLATFORM_NAME) + "." + str(type) + "." + str(columns[0]) + "." + str(columns[1]) + "." + str(columns[17]) + "." + "svctm" + " " + re.sub(",",".",columns[15]) +" "+ str(timestamp) + "\n")
                     netcat(PLATFORM_REPO, PLATFORM_REPO_PORT, PLATFORM_REPO_PROTOCOL,  str(PLATFORM) + "." + str(PLATFORM_NAME) + "." + str(type) + "." + str(columns[0]) + "." + str(columns[1]) + "." + str(columns[17]) + "." + "%util" + " " + re.sub(",",".",columns[16]) +" "+ str(timestamp) + "\n")
+                
                 logging.info("Finished metrics - %s" % log_stamp)
 
         cafs_iostat()
