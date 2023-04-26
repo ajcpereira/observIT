a4de717cb14d955eadd5b2ffdcada5a8cf7d33d6
ajcpereira <85030333+ajcpereira@users.noreply.github.com>
2023-04-26 18:42:01 +0100

    Update fs.py

--- functions/fs.py
+++ functions/fs.py
@@ -27,7 +27,7 @@ def fs(hostname, ssh, PLATFORM, PLATFORM_NAME, type, PLATFORM_REPO, PLATFORM_REP
 		
                 #response = stdout.read().decode('ascii')
                 response = stdout
-                response2 = stdout.read().decode()
+                response2 = stdout.read().decode('utf-16')
                 logging.info("SSH Output on function FS - %s" % response)
                 logging.info("SSH Output on function FS decode - %s" % response2)
              
