c821da9e2190b9a3904b3686cfe343631197cc22
ajcpereira <85030333+ajcpereira@users.noreply.github.com>
2023-04-26 10:56:13 +0100

    Update fs.py

--- functions/fs.py
+++ functions/fs.py
@@ -5,54 +5,38 @@ import logging
 
 def fs(hostname, ssh, PLATFORM, PLATFORM_NAME, type, PLATFORM_REPO, PLATFORM_REPO_PORT, PLATFORM_REPO_PROTOCOL, PLATFORM_USE_SUDO):
 
-	def cafs_iostat():
+        def cafs_iostat():
 	#########################
 	#CAFS_IOSTAT
 	#########################
-		log_stamp=time.time()
-		logging.info("Starting metrics - %s" % log_stamp)
-		CMD1="iostat -x 1 2"
+                log_stamp=time.time()
+                logging.info("Starting metrics - %s" % log_stamp)
+                CMD1="/opt/fsc/CentricStor/bin/rdNsdInfos -a > /tmp/stats_nsd.out"
+                CMD2="iostat -x -k 1 2| awk '!/^sd/'|awk -vN=2 '/avg-cpu/{++n} n>=N' > /tmp/stats_iostat.out"
+                CMD3="awk 'NR==FNR{a[$1]=$0; next} $3 in a{print a[$3],$0}' /tmp/stats_iostat.out /tmp/stats_nsd.out | awk '{print $18" "$1" "$2" "$3" "$4" "$5" "$6" "$78" "$9" "$10" "$11" "$12" "$13" "$14" "$15" "$16" "$17}' | sort"
 
-		if PLATFORM_USE_SUDO == "yes":
-			CMD1 = "sudo " + CMD1
+                if PLATFORM_USE_SUDO == "yes":
+                    CMD1 = "sudo " + CMD1
+                    CMD2 = "sudo " + CMD2
+                    CMD2 = "sudo " + CMD3
 
-		stdin, stdout, stderr = ssh.exec_command(CMD1)
-		timestamp = int(time.time())
+                ssh.exec_command(CMD1)
+                ssh.exec_command(CMD2)
+		stdin, stdout, stderr = ssh.exec_command(CMD3)
+                timestamp = int(time.time())
+                response = stdout.read().decode('ascii')
 
-		header=1
 		response = stdout.read().decode('ascii')
                 logging.info("SSH Output on function FS - %s" % response)
-		for line in response.splitlines():
-			if line.startswith("Device"):
-				header +=1
-				columns = line.split()
-				legend_metric_a=columns[5]
-				legend_metric_b=columns[11]
-			if len(line.split())==21 and not line.startswith("\n") and header == 2 and not line.startswith("Device"):
-				columns = line.split()
-				netcat(PLATFORM_REPO, PLATFORM_REPO_PORT, PLATFORM_REPO_PROTOCOL,  str(PLATFORM) + "." + str(PLATFORM_NAME) + "." + str(type) + "." + re.sub("\.","-", hostname) + "." + str(columns[0]) + "." + str(legend_metric_a) + " " + re.sub(",",".",columns[5]) +" "+ str(timestamp) + "\n")
-				netcat(PLATFORM_REPO, PLATFORM_REPO_PORT, PLATFORM_REPO_PROTOCOL,  str(PLATFORM) + "." + str(PLATFORM_NAME) + "." + str(type) + "." + re.sub("\.","-", hostname) + "." + str(columns[0]) + "." + str(legend_metric_b) + " " + re.sub(",",".",columns[11]) +" "+ str(timestamp) + "\n")
+                for line in response.splitlines():
+                    if len(line.split())==16 and not line.startswith("\n") and not line.startswith("Device"):
+                        columns = line.split()			
+                        netcat(PLATFORM_REPO, PLATFORM_REPO_PORT, PLATFORM_REPO_PROTOCOL,  str(PLATFORM) + "." + str(PLATFORM_NAME) + "." + str(type) + "." + str(columns[0]) + "." + str(columns[1]) + "." + str(columns[16]) + "." + "svctm" + " " + re.sub(",",".",columns[14]) +" "+ str(timestamp) + "\n")
+                        netcat(PLATFORM_REPO, PLATFORM_REPO_PORT, PLATFORM_REPO_PROTOCOL,  str(PLATFORM) + "." + str(PLATFORM_NAME) + "." + str(type) + "." + str(columns[0]) + "." + str(columns[1]) + "." + str(columns[16]) + "." + "%util" + " " + re.sub(",",".",columns[15]) +" "+ str(timestamp) + "\n")
 
-		logging.info("Finished metrics - %s" % log_stamp)
+                logging.info("Finished metrics - %s" % log_stamp)
 
-	cafs_iostat()
-		
-
-#	ssh.exec_command("/opt/fsc/CentricStor/bin/rdNsdInfos -a > /tmp/stats_nsd.out")
-#	ssh.exec_command("iostat -x -k 1 2| awk '!/^sd/'|awk -vN=2 '/avg-cpu/{++n} n>=N' > /tmp/stats_iostat.out")
-#	stdin, stdout, stderr = ssh.exec_command("awk 'NR==FNR{a[$1]=$0; next} $3 in a{print a[$3],$0}' /tmp/stats_iostat.out /tmp/stats_nsd.out | awk '{print $18" "$1" "$2" "$3" "$4" "$5" "$6" "$78" "$9" "$10" "$11" "$12" "$13" "$14" "$15" "$16" "$17}' | sort")
-#
-#	timestamp = int(time.time())
-#	response = stdout.read().decode('ascii')
-#
-#	for line in response.splitlines():
-#		if len(line.split())==16 and not line.startswith("\n") and not line.startswith("Device"):
-#			columns = line.split()
-#						
-#			netcat(PLATFORM_REPO, PLATFORM_REPO_PORT, PLATFORM_REPO_PROTOCOL,  str(PLATFORM) + "." + str(PLATFORM_NAME) + "." + str(type) + "." + str(columns[0]) + "." + str(columns[1]) + "." + str(columns[16]) + "." + "svctm" + " " + re.sub(",",".",columns[14]) +" "+ str(timestamp) + "\n")
-#			netcat(PLATFORM_REPO, PLATFORM_REPO_PORT, PLATFORM_REPO_PROTOCOL,  str(PLATFORM) + "." + str(PLATFORM_NAME) + "." + str(type) + "." + str(columns[0]) + "." + str(columns[1]) + "." + str(columns[16]) + "." + "%util" + " " + re.sub(",",".",columns[15]) +" "+ str(timestamp) + "\n")		
-
-
+        cafs_iostat()
 
 #########################
 #NEXT METRIC
