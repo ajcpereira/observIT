7edc5d98c42418d4e4a0e2e92e1f6c3fa034a19a
ajcpereira <85030333+ajcpereira@users.noreply.github.com>
2023-06-28 15:45:41 +0100

    Update secure_connect.py

--- functions/secure_connect.py
+++ functions/secure_connect.py
@@ -14,7 +14,7 @@ def secure_connect(hostname,bastion, user, type, host_keys, know_hosts, PLATFORM
         logging.info("Will SSH through bastion - %s" % log_stamp)
 
         try:
-            cmd_pkey_bastion = "cat /home/" + user + "/.ssh/id_rsa"
+            cmd_pkey_bastion = "cat $HOME/.ssh/id_rsa"
             ssh_bastion = Connection(host=bastion, user=user, port=22, connect_timeout=10, connect_kwargs={"key_filename": host_keys,})
             logging.info("Created ssh connection with bastion - %s" % bastion)
             pkey_bastion = ssh_bastion.run(cmd_pkey_bastion, hide=True).stdout.strip()
