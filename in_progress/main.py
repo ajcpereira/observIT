aaeac6162fd644bba3ba487924b7cbdc41217d5f
Alexandre Pereira <ajcpereira@pm.me>
2023-07-14 18:03:32 +0100

    in progress

--- in_progress/main.py
+++ in_progress/main.py
@@ -48,14 +48,15 @@ configfile="config.yaml"
 
 # every function must have alias as optional
 
-l_resources_types=["primergy", "primequest", "eternus_icp", "linux_os"]
-primergy_metrics=["cpu","mem","fs"]
-linux_os_metrics=["cpu", "mem", "fs"]
-eternus_icp_metrics=["fs"],["func_system_name","func_ip","func_alias", "func_pool", "func_bastion", "func_user", "func_metric", "func_host_keys", "func_know_hosts", "func_use_sudo","PLATFORM_REPO","PLATFORM_REPO_PORT","PLATFORM_REPO_PROTOCOL","PLATFORM_LOG","PLATFORM_LOGFILE"]
+l_resources_types=["eternus_icp", "linux_os"]
+l_linux_os_metrics=["cpu", "ram"], ["func_ip"], ["func_ip"]
+l_eternus_icp_metrics=["fs"],["func_system_name","func_ip","func_alias", "func_pool", "func_bastion", "func_user", "func_metric", "func_host_keys", "func_know_hosts", "func_use_sudo","PLATFORM_REPO","PLATFORM_REPO_PORT","PLATFORM_REPO_PROTOCOL","PLATFORM_LOG","PLATFORM_LOGFILE"]
 
 ########## SUPPORTED RESOURCES AND METRICS ######################################
 
 ########## GLOBAL PARAMETERS ####################################################
+# THEY ARE OVERWRITTEN AFTER READING THE CONFIG FILE                            #
+#################################################################################
 PLATFORM_REPO="graphite"
 PLATFORM_REPO_PORT=2003
 PLATFORM_REPO_PROTOCOL="tcp"
@@ -106,41 +107,52 @@ def f_readglobalparametersconfigfile():
 
 ########## FUNCTION GET VALUES TO BUILD TASKS FROM CONFIG FILE ##################
 def f_readconfigfile(configdata):
+
+    ########## FUNCTION VALIDATE RESOURCES AND METRICS FROM CONFIG FILE ##########
+    def f_validate_resources_metrics(input_resources, input_metrics):
+        if input_resources in l_resources_types:
+            print (input_resources)
+            if input_metrics in eval("l_" + input_resources + "_metrics")[0]:
+                pos_index=eval("l_" + input_resources + "_metrics")[0].index(input_metrics)
+                print(input_metrics)
+                for i in range(1,len(eval("l_" + input_resources + "_metrics")[pos_index + 1])):
+                    print(eval(eval("l_" + input_resources + "_metrics")[pos_index + 1][i]))
+#                    if eval(eval("l_" + input_resources + "_metrics")[pos_index + 1][i]):
+#                        print(eval(eval("l_" + input_resources + "_metrics")[pos_index + 1][i]))
+                        #print(eval("l_" + input_resources + "_metrics")[pos_index + 1][i] + " is set")
+#                    else:
+#                        print (eval("l_" + input_resources + "_metrics")[pos_index + 1][i] + " is not set, error in code")
+            else:
+                print("metric not valid - %s" % input_metrics)
+        else:
+            print("resource not valid - %s" % input_resources)
+    ########## FUNCTION VALIDATE RESOURCES AND METRICS FROM CONFIG FILE ##########
+
     for system in configdata['systems']:
         func_system_name=system['name']
         for config in system['config']:
             func_resource=config['resources_types']
-            for metric in config['metrics']:
-                func_metric=metric['name']
-            for ip in config['ips']:
-                func_ip=ip['ip']
-                func_alias=ip['alias']
             parameters = config['parameters']
             if 'host_keys' in parameters:
                 func_host_keys=parameters['host_keys']
             if 'user' in parameters:
                 func_user=parameters['user']
-            if 'know_hosts' in parameters:
-                func_know_hosts=parameters['know_hosts']
+            if 'known_hosts' in parameters:
+                func_known_hosts=parameters['known_hosts']
             if 'poll' in parameters:
                 func_poll=parameters['poll']
             if 'use_sudo' in parameters:
                 func_use_sudo=parameters['use_sudo']
+            for metric in config['metrics']:
+                func_metric=metric['name']
+                for ip in config['ips']:
+                    func_ip=ip['ip']
+                    func_alias=ip['alias']
+                    print(func_system_name + func_resource + func_user + func_host_keys + func_known_hosts + str(func_poll) + func_metric + func_ip + func_alias)
+                    f_validate_resources_metrics(func_resource, func_metric)
+
 
 ########## FUNCTION GET VALUES TO BUILD TASKS FROM CONFIG FILE ##################
-
-
-########## FUNCTION VALIDATE RESOURCES AND METRICS FROM CONFIG FILE ##########
-def f_validate_resources_metrics(input_resources, input_metrics):
-    if input_resources in l_resources_types:
-        print (input_resources)
-        if input_metrics in eval(input_resources + "_metrics"):
-            print(input_metrics)
-        else:
-            print("metric not valid - %s" % input_metrics)
-    else:
-        print("resource not valid - %s" % input_resources)
-########## FUNCTION VALIDATE RESOURCES AND METRICS FROM CONFIG FILE ##########
 
 #################################################################################
 #                                                                               #
