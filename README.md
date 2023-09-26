2bdfa5cac848207ae4436b8c529aeec6151530e3
Alexandre Pereira <ajcpereira@pm.me>
2023-09-26 14:17:06 +0100

    Version 1

--- README.md
+++ README.md
@@ -1,71 +1,99 @@
-## FJ Data Collection
-### Collects data and populate it in Graphite, using the native integration of Grafana dashboards can be drawn.
-### Only uses ssh at the moment to collect data, but SNMP and RestAPI will be supported
 
+
+#                       IDENTIFICATION DIVISION
+
+
+
+# FJ Data Collector
+
+This is an aggregator for data collection that populates data in Graphiteapp and since Grafana integrates with Graphite you can draw your graphs in Grafana.
+
+The Collector is a scheduler that collects information based in a YAML file, each schedule runs on it's own thread and it's completly free:
+https://github.com/ajcpereira/fj-collector
+
+Regarding the other 2 mention applications be aware of their licenses if you wish to use in your organization.
+Our makefile installs both but they are not part of the code of the fj-collector, so you should check your policies or maybe you already have them in your organization so you can use them:
+
+Graphite - https://graphiteapp.org/ - 26/07/2023: (...) Graphite was originally designed and written by Chris Davis at Orbitz in 2006 as side project that ultimately grew to be their foundational monitoring tool. In 2008, Orbitz allowed Graphite to be released under the open source Apache 2.0 license.(...)
+
+Grafana - https://grafana.com/licensing/ - 26/07/2023: (...) On April 20, 2021, Grafana Labs announced that going forward, our core open source projects will be moving from the Apache License v2.0 to AGPLv3.
+Users who don’t intend to modify Grafana code can simply use our Enterprise download. This is a free-to-use, proprietary-licensed, compiled binary that matches the features of the AGPL version, and can be upgraded to take advantage of all the commercial features in Grafana Enterprise (Enterprise plugins, advanced security, reporting, support, and more) with the purchase of a license key. (...)
 
 ### Requirements
 
-Requires docker
+Requires linux
+
+Requires podman (also dnsmasq)
+   In Ubuntu there's a bug with podman - https://bugs.launchpad.net/ubuntu/+source/libpod/+bug/2024394 - please rollback if not solved by the time you are using - apt install podman=3.4.4+ds1-1ubuntu1
 
 Requires git
 
-Requires folder /opt/fj-collector/collector/logs
+Internet access to github.com
 
-Requires folder /opt/fj-collector/collector/config where it must include the "collector.yml" file:
+Will install under folder /opt/fj-collector
+
+Your user must have permissions in /opt and run podman
 
 ### Installation Procedure
 
+Clone the git repository:
+
 ````
 git clone https://github.com/ajcpereira/fj-collector.git
-cd fj-collector
-docker-compose up
 ````
 
-inside /opt/fj-collector/collector/config create the following collector.yaml
+Now, just run the install
+
+````
+cd fj-collector
+make setup
+make start
+````
+
+edit /opt/fj-collector/collector/config/collector.yaml
+
+For each IP that you access through ssh you will need the private key.
+On the previous file (collector.yaml) you can specifie it's location (host_keys entry)
+Be aware that since the code runs inside a container the collector.yaml uses the path inside of the container, so:
+  /collector/fj-collector/config/
+  is your
+  /opt/fj-collector/collector/config/
+
+  We recommend you use relative paths:
+    host_keys: keys/id_rsa
+    logfile: logs/fj-collector.log
  
 ### collector.yaml
 ````
-solution: 
-  platform: 
-    - 
-      type: CS8000    # Only CS8000 supported atm
-      name: MYCS8000
-      resources: 
-        type: 
-          - fs  # fs=filesystem only supported for now
-        ip: 
-          - 10.0.2.15
-        user: report
-        proxy: # Leave empty if not using otherwise insert IP/FQDN/HOSTNAME
-        poll: 1 # Minute
-    - 
-      type: CS8000    # Only CS8000 supported
-      name: SecondCS8000
-      resources: 
-        type: 
-          - fs # fs=filesystem only supported for now
-        ip: 
-          - localhost
-        user: report
-        proxy: # Not supported yet
-        poll: 2 # Minutes
-
-parameters:
+systems:
+  - name: MYCS8000
+    resources_types: eternus_icp
+    config:
+      parameters:
+          user: alex
+          host_keys: keys/id_rsa
+          poll: 1
+          bastion: 172.21.69.166
+      metrics:
+          - name: fs
+      ips:
+          - ip: 172.21.69.166
+            alias: icp0
+          - ip: 172.21.69.166
+            ip_host_keys: keys/id_rsa
+            ip_bastion: 172.21.69.166
+global_parameters:
   repository: graphite
   repository_port: 2003
   repository_protocol: tcp
-  host_keys: "/collector/fj-collector/config/id_rsa" # You will have to use a private key - ssh-copy-id username@remote_host, must be done previously
-  known_hosts: "/collector/fj-collector/config/known_hosts" # If host is unknown the process will fail
-  use_sudo: no
-  log: INFO # The log level - DEBUG, INFO, WARNING, ERROR and CRITICAL
+  collector_root: fj-collector
+  loglevel: INFO
+  logfile: logs/fj-collector.log
 ````  
 
 ### Metrics
 
-Atm we are collecting the iostat for each CS HE filesystem, so you will get filesystem, device multipath and raw device with metrics for:
-type: fs
-  - svctm
-  - %util
+Consult the README inside the folder functions
 
 ### Metrics Retention
 
@@ -85,5 +113,3 @@ More info can be found - https://graphite.readthedocs.io/en/latest/config-carbon
 
 ### Architecture
 ![Design](https://github.com/ajcpereira/reporting/raw/main/img/design.png)
-
-
--- main.py
+++ main.py
@@ -1,55 +1,116 @@
-import sys
-sys.path.append("functions")
-import yaml
-import time
-from functions.secure_connect import secure_connect
-import schedule
-import logging
+#################################################################################
+#                                                                               #
+#                       IDENTIFICATION DIVISION                                 #
+#                                                                               #
+#################################################################################
+# This is the main program from the FJ Collector
+# it receives inputs from a YAML configfile to collect metrics from
+# different sources
+#
+# This is not a comercial product and it's 'as is' is basically for PoC that a 
+# unique tool can get the most relevant data from a Datacenter and how easy
+# it can be
+#
+# Also, will allow to the ones intervening in this process to acquire knowledge
+# in different protocols and command lines to acquire the needed info
 
-configfile = "/collector/fj-collector/config/collector.yml"
+#################################################################################
+#                                                                               #
+#                       ENVIRONMENT DIVISION                                    #
+#                                                                               #
+#################################################################################
+import sys, os, logging, time
+from threading import Thread, Event
+from functions_core.yaml_validate import *
+from functions import *
 
-# Open the YAML file and read it
-with open(configfile, 'r') as f:
-  try:
-    configdata = yaml.safe_load(f)
-    f.close
-  except FileNotFoundError:
-    print("Sorry, the file %s does not exist" % configfile)
+#################################################################################
+#                                                                               #
+#                       DATA DIVISION                                           #
+#                                                                               #
+#################################################################################
 
-# CREATE CONSTANTS BASED IN PARAMETERS FROM YAML
-  PLATFORM_REPO=configdata["parameters"]["repository"]
-  PLATFORM_REPO_PORT=configdata["parameters"]["repository_port"]
-  PLATFORM_REPO_PROTOCOL=configdata["parameters"]["repository_protocol"]
-  PLATFORM_HOSTKEYS=configdata["parameters"]["host_keys"]
-  PLATFORM_USE_SUDO=configdata["parameters"]["use_sudo"]
-  PLATFORM_KNOW_HOSTS=configdata["parameters"]["known_hosts"]
-  PLATFORM_LOG="logging." + configdata["parameters"]["log"]
+configfile_default = "config/config.yaml"
+event = Event()
 
-# Start Logging Facility
-logging.basicConfig(filename='/collector/fj-collector/logs/fj-collector.log', level=eval(PLATFORM_LOG))
+#################################################################################
+#                                                                               #
+#                       PROCEDURE DIVISION                                      #
+#                                                                               #
+#################################################################################
 
-# GET Time for logging YAML
-log_stamp=time.ctime()
+########## FUNCTION LAUNCH A THREAD FOR EACH SCHEDULE ###########################
 
-# PROCESS AND EXECUTE EACH LINE FROM YAML
-logging.info("Starting YAML Processing - %s" % log_stamp)
+def run_threaded(**args) -> None:
 
-for i in range(len(configdata['solution']['platform'])):
-  if configdata['solution']['platform'][i]['type'] == "CS8000":
-    PLATFORM=(configdata['solution']['platform'][i]['type'])
-    PLATFORM_NAME=(configdata['solution']['platform'][i]['name'])
-    for z in range(len(configdata['solution']['platform'][i]['resources']['type'])):
-      if configdata['solution']['platform'][i]['resources']['type'][z] == "fs":
-        for x in range(len(configdata['solution']['platform'][i]['resources']['ip'])):
-          var_configdata_current=configdata['solution']['platform'][i]['resources']
-          schedule.every(var_configdata_current["poll"]*60).seconds.do(secure_connect, var_configdata_current["ip"][x], var_configdata_current["proxy"], var_configdata_current["user"], var_configdata_current["type"][z], PLATFORM_HOSTKEYS, PLATFORM_KNOW_HOSTS, PLATFORM, PLATFORM_NAME, PLATFORM_REPO, PLATFORM_REPO_PORT, PLATFORM_REPO_PROTOCOL, PLATFORM_USE_SUDO)
-      elif configdata['solution']['platform'][i]['resources']['type'][z] == "channel":
-        for x in range(len(configdata['solution']['platform'][i]['resources']['ip'])):
-          print("channel" + configdata['solution']['platform'][i]['resources']['ip'])
-      else:
-          print("No valid option for type")
-logging.info("Finished YAML Processing - %s" % log_stamp)
+    while True:
+        event.wait(timeout=args['poll']*60)
+        logging.debug("Will run_thread with %s" % args)
+        if event.is_set():
+            logging.warning("Exited run_thread")
+            break
+        #Thread(target=eval(args['func']+"."+args['func']), kwargs=args).start()
+        Thread(target=eval(args['resources_types']+"."+args['func']), kwargs=args).start()
 
-while True:
-    schedule.run_pending()
-    time.sleep(10)
+########## FUNCTION LAUNCH A THREAD FOR EACH SCHEDULE ###########################
+
+########## FUNCTION FOR EACH METRIC LAUNCH THREADS  #############################
+
+def launch_thread(result_dicts):
+    for result_dict in result_dicts:
+        logging.debug("Will launch tread %s" % result_dict)
+        Thread(target=run_threaded, kwargs=result_dict).start()
+
+########## FUNCTION FOR EACH METRIC LAUNCH THREADS  #############################
+
+#################################################################################
+#                                                                               #
+#                       MAIN                                                    #
+#                                                                               #
+#################################################################################
+
+if __name__ == "__main__":
+    
+    config, orig_mtime, configfile_running = configfile_read(sys.argv, configfile_default)
+    result_dicts, global_parms = create_metric_ip_dicts(config)
+    
+    ########## BEGIN - Start Logging Facility #######################################
+    logging.basicConfig(filename=global_parms['logfile'], level=eval("logging."+global_parms['loglevel']), format='%(asctime)s %(levelname)s %(module)s %(threadName)s %(message)s', force=True)
+    ########## END - Start Logging Facility #########################################    
+
+    ########## BEGIN - Log configfile start processing ##############################
+    logging.info("Starting Collector")
+    ########## END - Log configfile start processing ################################
+  
+    launch_thread(result_dicts)
+
+    while True:
+        time.sleep(1)
+
+        if orig_mtime < os.path.getmtime(configfile_running):
+            logging.info("Configfile changed, will reload")
+
+            event.set()
+            
+            orig_mtime = os.path.getmtime(configfile_running)
+            config, orig_mtime, configfile_running = configfile_read(sys.argv, configfile_default)
+            result_dicts, global_parms = create_metric_ip_dicts(config)
+            
+            logging.debug("File changed will load %s" % result_dicts)
+
+            for handler in logging.root.handlers[:]:
+                logging.root.removeHandler(handler)
+            try:             
+                logging.basicConfig(filename=global_parms['logfile'], level=eval("logging."+global_parms['loglevel']), format='%(asctime)s %(levelname)s %(module)s %(threadName)s %(message)s', force=True)
+            except Exception as msgerr:
+                logging.fatal("Failed to change logging basicConfig %s" % msgerr)
+                sys.exit()
+            
+            time.sleep(5)
+            event.clear()
+            launch_thread(result_dicts)
+            
+            logging.info("Configfile reloaded")
+
+
+            
