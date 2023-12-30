6a0bf56c43181b28bc9b902d2dfc230db60d8196
Alexandre Pereira <ajcpereira@pm.me>
2023-12-30 13:42:10 +0000

    Added metrics MEDIAS

--- functions/eternus_cs8000.py
+++ functions/eternus_cs8000.py
@@ -286,49 +286,47 @@ def eternus_cs8000_medias(**args):
     logging.debug("Starting metrics processing on MEDIAS")
 
 
-##############################
-    tapename = None
-    count_unused = 0
-    count_used = 0
-    count_another_state = 0
-    record=[]
+    ##############################
+    library = {}
+    record = []
     
-    for line in response.splitlines():
+    for line in cmd1.splitlines():
         columns = line.split()
-    
-        if line.startswith("Tapelibrary"):    
-            if tapename != None:
-          
-                record = record + [{"measurement": "drives", "tags": {"system": args['name'], "resource_type": args['resources_types'], "host": hostname, "tapename": tapename },
-                                  "fields": {"total": float(count_used+count_unused+count_another_state), "used": float(count_used), "other": float(count_another_state)},
-                                  "time": timestamp}]
-    
-                print("tapename %s total drives %s used %s unused %s other %s" % (tapename, count_used+count_unused+count_another_state, count_used, count_unused, count_another_state))
-                count_unused = 0
-                count_used = 0
-                count_another_state = 0
-                tapename = str(columns[1])
-            else:
-                tapename = str(columns[1])
-        elif line.startswith("PLS") or line.startswith("pos"):
+        if line.startswith("pos"):
             continue
         else:
-            if str(columns[2]) == "unused":
-                count_unused = count_unused + 1
-            elif str(columns[2]) == "occupied":
-                count_used = count_used + 1
+            # Lib / Count Medias / Total Cap / Total Valid / Val% / Clean / Inacessible / Fault    
+            if str(columns[2]) not in library.keys():
+                if str(columns[4][0]) == 'i' and not str(columns[1]).startswith("CLN"):
+                    library[str(columns[2])]=[str(columns[2]),0,0,0,0.0,0,1,0]
+                elif str(columns[4][0]) == 'f' and not str(columns[1]).startswith("CLN"):
+                    library[str(columns[2])]=[str(columns[2]),0,0,0,0.0,0,0,1]
+                elif str(columns[1]).startswith("CLN"):
+                    library[str(columns[2])]=[str(columns[2]),0,float(columns[8]),float(columns[9]),0.0,1,0,0]
+                elif float(columns[8]) > 0 and not str(columns[1]).startswith("CLN"):
+                    library[str(columns[2])]=[str(columns[2]),1,float(columns[8]),float(columns[9]),float(columns[10]),0,0,0]
+                else:
+                    logging.error("This line requires development analysis -  %s" % columns)
             else:
-                count_another_state = count_another_state + 1
-    
-    record = record + [{"measurement": "drives", "tags": {"system": args['name'], "resource_type": args['resources_types'], "host": hostname, "tapename": tapename },
-                                  "fields": {"total": float(count_used+count_unused+count_another_state), "used": float(count_used), "other": float(count_another_state)},
+                if str(columns[4][0]) == 'i' and not str(columns[1]).startswith("CLN"):
+                    library[str(columns[2])]=[str(columns[2]),int(library[str(columns[2])][1])+0,float(library[str(columns[2])][2]),float(library[str(columns[2])][3]),float(library[str(columns[2])][4]),int(library[str(columns[2])][5])+0,int(library[str(columns[2])][6])+1,int(library[str(columns[2])][7])+0]
+                elif str(columns[4][0]) == 'f' and not str(columns[1]).startswith("CLN"):
+                    library[str(columns[2])]=[str(columns[2]),int(library[str(columns[2])][1])+0,float(library[str(columns[2])][2]),float(library[str(columns[2])][3]),float(library[str(columns[2])][4]),int(library[str(columns[2])][5])+0,int(library[str(columns[2])][6])+0,int(library[str(columns[2])][7])+1]
+                elif str(columns[1]).startswith("CLN"):
+                    library[str(columns[2])]=[str(columns[2]),int(library[str(columns[2])][1])+0,float(library[str(columns[2])][2]),float(library[str(columns[2])][3]),float(library[str(columns[2])][4]),int(library[str(columns[2])][5])+1,int(library[str(columns[2])][6])+0,int(library[str(columns[2])][7])+0]
+                elif float(columns[8]) > 0 and not str(columns[1]).startswith("CLN"):
+                    library[str(columns[2])]=[str(columns[2]),int(library[str(columns[2])][1])+1,float(library[str(columns[2])][2])+float(columns[8]),float(library[str(columns[2])][3])+float(columns[9]),float(library[str(columns[2])][4])+float(columns[10]),int(library[str(columns[2])][5])+0,int(library[str(columns[2])][6])+0,int(library[str(columns[2])][7])+0]
+                else:
+                    logging.error("This line requires development analysis -  %s" % columns)
+
+    for line in library.values():
+        if line[4] != 0 and line[1] != 0:
+            line[4]=line[4]/line[1]
+
+        record = record + [{"measurement": "medias", "tags": {"system": args['name'], "resource_type": args['resources_types'], "host": hostname, "tapename": line[0] },
+                                  "fields": {"Total Medias": line[1], "Total Cap": line[2], "Total Val": line[3], "Val %": line[4], "Total Clean Medias": line[5], "Total Ina": line[6], "Total Fault": line[7]},
                                   "time": timestamp}]
-    
-    print("tapename %s total drives %s used %s unused %s other %s" % (tapename, count_used+count_unused+count_another_state, count_used, count_unused, count_another_state))
-    
-    
-    print(record)
-########################
+    ########################
 
     # Send Data to InfluxDB
     logging.debug("Data to be sent to DB by medias %s" % record)
