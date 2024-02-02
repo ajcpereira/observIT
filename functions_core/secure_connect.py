eb2daa30dc0df816a38d342b0cbf65a15c75030f
Alexandre Pereira <ajcpereira@pm.me>
2024-02-02 23:50:07 +0000

    #45 session manager bug fix

--- functions_core/secure_connect.py
+++ functions_core/secure_connect.py
@@ -6,7 +6,8 @@ import threading
 
 class Secure_Connect():
     existing_sessions = []
-    
+    global_lock = threading.Lock()
+
     @staticmethod 
     def manage_sessions(session_key: list):
         timestamp = time.time()
@@ -48,106 +49,100 @@ class Secure_Connect():
 
     def __init__(self, param_ip, bastion, user, host_keys):
 
-
-
         session_key = [param_ip, bastion, user, host_keys, self, time.time()]
 
         logging.debug(f"my session_key is {session_key[:4]}")
         logging.debug(f"my existing sessions are {Secure_Connect.existing_sessions}")
         
-        self.lock = threading.Lock()
-
-        with self.lock:
+        with Secure_Connect.global_lock:
             ret_value = Secure_Connect.manage_sessions(session_key)
             logging.debug(f"Returned value from function is {ret_value}")
             if ret_value:
                 logging.debug(f"Will return value:{ret_value}")
-                return ret_value
+                self.ssh = ret_value.ssh
             else:
-                Secure_Connect.existing_sessions.append(session_key)
-                logging.debug("No value was returned so let's run all the process")
-
-
-            logging.debug("Class Secure_connect Started")
-            self.bastion = bastion
-            if bastion:
-                logging.debug("Class Secure_connect with bastion Started")
-                cmd_pkey_bastion = "cat $HOME/.ssh/id_rsa"
-                # Open connection to bastion
-                try:
-                    logging.debug("Values ip %s, bastion %s, user %s and host_keys %s" % (param_ip, bastion, user, host_keys))
-                    self.ssh_bastion = fabric2.Connection(host=str(bastion), user=user, port=22, connect_timeout=12, connect_kwargs={"key_filename": host_keys, "banner_timeout":12, "auth_timeout":12, "channel_timeout":12,})
-                    self.ssh_bastion.open()
-                    logging.debug("Got session for bastion")
-                except Exception as msgerror:
-                    logging.error("Failed fabric2 - %s" % msgerror)
-                    if hasattr(self,'ssh'):
-                        self.ssh.close()
-                        del self.ssh
-                    if hasattr(self, 'ssh_bastion'):
-                        self.ssh_bastion.close()
-                        del self.ssh_bastion
-                    del self
-                # Get pkey on bastion
-                try:
-                    logging.debug("Will get pkey")
-                    pkey_bastion = self.ssh_bastion.run(cmd_pkey_bastion, hide=True).stdout.strip()
-                    pkey_trunk = pkey_bastion[0:50]
-                    logging.debug("Got the pkey %s" % pkey_trunk)
-                except Exception as msgerror:
-                    logging.error("Failed to get pkey form bastion - %s" % msgerror)
-                    if hasattr(self,'ssh'):
-                        self.ssh.close()
-                        del self.ssh
-                    if hasattr(self, 'ssh_bastion'):
-                        self.ssh_bastion.close()
-                        del self.ssh_bastion
-                    del self
-                # Write the private key contents to a temporary file
-                with tempfile.NamedTemporaryFile(delete=False, buffering=- 1) as f:
-                    f.write(pkey_bastion.encode())
-                    private_key_file = f.name
-                logging.debug("Wrote the key in temp file")
-                # connection to host using pkey
-                try:
-                    self.ssh = fabric2.Connection(host=str(param_ip), user=user, port=22, connect_timeout=12, connect_kwargs={"key_filename": private_key_file,"banner_timeout":12, "auth_timeout":12, "channel_timeout":12,}, gateway=self.ssh_bastion)
-                    logging.debug("Created ssh connection with hostname - %s - through bastion - %s" % (param_ip, bastion))
-                except Exception as msgerror:
-                    logging.error("Class Secure_Connect with bastion FAILED - %s" % msgerror)
-                    if hasattr(self,'ssh'):
-                        self.ssh.close()
-                        del self.ssh
-                    if hasattr(self, 'ssh_bastion'):
-                        self.ssh_bastion.close()
-                        del self.ssh_bastion
-                    del self
-                # open connection through bastion to host
-                try:
-                    logging.debug("will open session")
-                    self.ssh.open()
-                    logging.debug("Class Secure_connect open ok with bastion, will return session - %s" % self)
-                except Exception as msgerror:
-                    logging.error("Class Secure_Connect with bastion FAILED - %s - for ip %s" % (msgerror, param_ip))
-                    if hasattr(self,'ssh'):
-                        self.ssh.close()
-                        del self.ssh
-                    if hasattr(self, 'ssh_bastion'):
-                        self.ssh_bastion.close()
-                        del self.ssh_bastion
-                    del self
-            else:
-                logging.debug("Class Secure_connect without bastion Started")
-                try:
-                    self.ssh = fabric2.Connection(host=param_ip, user=user, port=22, connect_timeout=12, connect_kwargs={"key_filename": host_keys,"banner_timeout":12, "auth_timeout":12, "channel_timeout":12,})
-                    self.ssh.open()
-                    logging.debug("Class Secure_connect open ok with bastion, will return session - %s" % self)
-                except Exception as msgerror:
-                    logging.error("Class Secure FAILED - %s - for ip %s" % (msgerror, param_ip))
-                    if hasattr(self,'ssh'):
-                        self.ssh.close()
-                        del self.ssh
-                    del self
-                 
+                logging.debug("Class Secure_connect Started - No value was returned so let's run all the process")
+                self.bastion = bastion
+                if bastion:
+                    logging.debug("Class Secure_connect with bastion Started")
+                    cmd_pkey_bastion = "cat $HOME/.ssh/id_rsa"
+                    # Open connection to bastion
+                    try:
+                        logging.debug("Values ip %s, bastion %s, user %s and host_keys %s" % (param_ip, bastion, user, host_keys))
+                        self.ssh_bastion = fabric2.Connection(host=str(bastion), user=user, port=22, connect_timeout=12, connect_kwargs={"key_filename": host_keys, "banner_timeout":12, "auth_timeout":12, "channel_timeout":12,})
+                        self.ssh_bastion.open()
+                        logging.debug("Got session for bastion")
+                    except Exception as msgerror:
+                        logging.error("Failed fabric2 - %s" % msgerror)
+                        if hasattr(self,'ssh'):
+                            self.ssh.close()
+                            del self.ssh
+                        if hasattr(self, 'ssh_bastion'):
+                            self.ssh_bastion.close()
+                            del self.ssh_bastion
+                        del self
+                    # Get pkey on bastion
+                    try:
+                        logging.debug("Will get pkey")
+                        pkey_bastion = self.ssh_bastion.run(cmd_pkey_bastion, hide=True).stdout.strip()
+                        pkey_trunk = pkey_bastion[0:50]
+                        logging.debug("Got the pkey %s" % pkey_trunk)
+                    except Exception as msgerror:
+                        logging.error("Failed to get pkey form bastion - %s" % msgerror)
+                        if hasattr(self,'ssh'):
+                            self.ssh.close()
+                            del self.ssh
+                        if hasattr(self, 'ssh_bastion'):
+                            self.ssh_bastion.close()
+                            del self.ssh_bastion
+                        del self
+                    # Write the private key contents to a temporary file
+                    with tempfile.NamedTemporaryFile(delete=False, buffering=- 1) as f:
+                        f.write(pkey_bastion.encode())
+                        private_key_file = f.name
+                    logging.debug("Wrote the key in temp file")
+                    # connection to host using pkey
+                    try:
+                        self.ssh = fabric2.Connection(host=str(param_ip), user=user, port=22, connect_timeout=12, connect_kwargs={"key_filename": private_key_file,"banner_timeout":12, "auth_timeout":12, "channel_timeout":12,}, gateway=self.ssh_bastion)
+                        logging.debug("Created ssh connection with hostname - %s - through bastion - %s" % (param_ip, bastion))
+                    except Exception as msgerror:
+                        logging.error("Class Secure_Connect with bastion FAILED - %s" % msgerror)
+                        if hasattr(self,'ssh'):
+                            self.ssh.close()
+                            del self.ssh
+                        if hasattr(self, 'ssh_bastion'):
+                            self.ssh_bastion.close()
+                            del self.ssh_bastion
+                        del self
+                    # open connection through bastion to host
+                    try:
+                        logging.debug("will open session")
+                        self.ssh.open()
+                        logging.debug("Class Secure_connect open ok with bastion, will return session - %s" % self)
+                    except Exception as msgerror:
+                        logging.error("Class Secure_Connect with bastion FAILED - %s - for ip %s" % (msgerror, param_ip))
+                        if hasattr(self,'ssh'):
+                            self.ssh.close()
+                            del self.ssh
+                        if hasattr(self, 'ssh_bastion'):
+                            self.ssh_bastion.close()
+                            del self.ssh_bastion
+                        del self
+                    Secure_Connect.existing_sessions.append(session_key)
+                else:
+                    logging.debug("Class Secure_connect without bastion Started")
+                    try:
+                        self.ssh = fabric2.Connection(host=param_ip, user=user, port=22, connect_timeout=12, connect_kwargs={"key_filename": host_keys,"banner_timeout":12, "auth_timeout":12, "channel_timeout":12,})
+                        self.ssh.open()
+                        logging.debug("Class Secure_connect open ok with bastion, will return session - %s" % self)
+                    except Exception as msgerror:
+                        logging.error("Class Secure FAILED - %s - for ip %s" % (msgerror, param_ip))
+                        if hasattr(self,'ssh'):
+                            self.ssh.close()
+                            del self.ssh
+                        del self
+                    Secure_Connect.existing_sessions.append(session_key)
+                     
 
     def ssh_run(self, cmd):
         try:
@@ -165,5 +160,5 @@ class Secure_Connect():
             del self
     
     def ssh_del(self):
-        with self.lock:
+        with Secure_Connect.global_lock:
             Secure_Connect.manage_sessions(None)
