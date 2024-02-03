181a8d9d8b79fdc0e251018a7bbe1c0a99375482
Alexandre Pereira <ajcpereira@pm.me>
2024-02-03 17:45:37 +0000

    minor fixes in secure_connect

--- functions_core/secure_connect.py
+++ functions_core/secure_connect.py
@@ -1,45 +1,43 @@
-import fabric2
-import logging
-import tempfile
-import time
-import threading
+import fabric2, logging, tempfile, time, threading
 
+# Will be used for a multi-thread environment to manage ssh connections
 class Secure_Connect():
-    existing_sessions = []
+
+    # Will keep track of ssh sessions, if parameters are the same will not open new, avoiding exhausting ssh sessions in servers
+    active_sessions = []
+    # Since this class will be used in a multi-thread environment to avoid race conditions we need to lock resources like the method manage_sessions() and the tuple active_sessions
     global_lock = threading.Lock()
 
     @staticmethod 
     def manage_sessions(session_key: list):
-        timestamp = time.time()
+        timestamp_now = time.time()
         keep_sessions = []
         valid_session = []
-        logging.debug(f"Managing sessions for this session keys:{session_key} and this content for existing_sessions {Secure_Connect.existing_sessions}")
+        
+        logging.debug(f"Managing sessions for this session keys:{session_key} and this content for active_sessions {Secure_Connect.active_sessions}")
 
-        if not len(Secure_Connect.existing_sessions) == 0:
-            logging.debug(f"Existing sessions are not empty {Secure_Connect.existing_sessions}")
-            for value in Secure_Connect.existing_sessions:
-                if (timestamp - value[5]) <= 55:
-                    logging.debug(f"Still a valid session with time below 55s {timestamp - value[5]}")
+        if not len(Secure_Connect.active_sessions) == 0:
+            logging.debug(f"Existing sessions are not empty {Secure_Connect.active_sessions}")
+            for value in Secure_Connect.active_sessions:
+                if abs(timestamp_now - value[5]) <= 55 and value[4].ssh.is_connected:
+                    logging.debug(f"Still a valid session with time below 55s {abs(timestamp_now - value[5])} and is_connected for session {value}")
                     keep_sessions.append(value)
-                    logging.debug(f"keeping session {value}")
-                    if session_key:
-                        logging.debug(f"The value is {value[:4]} and the session_key is {session_key[:4]} will check if they are equal")
-                        if value[:4] == session_key[:4]:
-                            logging.debug(f"This is the object to keep {value[4]}")
-                            valid_session = value[4]
-                            logging.debug(f"The object type for valid_session is {type(valid_session)}")
+                    if session_key and value[:4] == session_key[:4]:
+                        logging.debug(f"This is the instance to be returned {value[4]}")
+                        valid_session = value[4]
                 else:
-                    logging.debug(f"will close sessions not longer valid {value}")
-                    mysession=value[4]
-                    if hasattr(mysession,'ssh'):
-                        mysession.ssh.close()
-                        del mysession.ssh
-                        logging.debug("Closed session on Class Secure_connect - %s" % mysession)
-                    if hasattr(mysession, 'ssh_bastion'):
-                        mysession.ssh_bastion.close()
-                        del mysession.ssh_bastion
-                        logging.debug("Closed bastion session on Class Secure_connect - %s" % mysession)
-            Secure_Connect.existing_sessions = keep_sessions
+                    logging.debug(f"Will close sessions not longer valid {value}")
+                    invalid_session=value[4]
+                    if hasattr(invalid_session,'ssh'):
+                        invalid_session.ssh.close()
+                        del invalid_session.ssh
+                        logging.debug("Closed session on Class Secure_connect - %s" % invalid_session)
+                    if hasattr(invalid_session, 'ssh_bastion'):
+                        invalid_session.ssh_bastion.close()
+                        del invalid_session.ssh_bastion
+                        logging.debug("Closed bastion session on Class Secure_connect - %s" % invalid_session)
+            Secure_Connect.active_sessions = keep_sessions
+            logging.debug(f"The active sessions are:\n{Secure_Connect.active_sessions}")
             if valid_session:
                 logging.debug(f"will pass session {valid_session}")
                 return valid_session
@@ -52,7 +50,7 @@ class Secure_Connect():
         session_key = [param_ip, bastion, user, host_keys, self, time.time()]
 
         logging.debug(f"my session_key is {session_key[:4]}")
-        logging.debug(f"my existing sessions are {Secure_Connect.existing_sessions}")
+        logging.debug(f"my existing sessions are {Secure_Connect.active_sessions}")
         
         with Secure_Connect.global_lock:
             ret_value = Secure_Connect.manage_sessions(session_key)
@@ -60,9 +58,13 @@ class Secure_Connect():
             if ret_value:
                 logging.debug(f"Will return value:{ret_value}")
                 self.ssh = ret_value.ssh
+                if hasattr(ret_value, 'ssh_bastion'):
+                        self.ssh_bastion = ret_value.ssh_bastion
             else:
+                # No active sessions available will create
                 logging.debug("Class Secure_connect Started - No value was returned so let's run all the process")
                 self.bastion = bastion
+                # Open connection with bastion
                 if bastion:
                     logging.debug("Class Secure_connect with bastion Started")
                     cmd_pkey_bastion = "cat $HOME/.ssh/id_rsa"
@@ -128,7 +130,8 @@ class Secure_Connect():
                             self.ssh_bastion.close()
                             del self.ssh_bastion
                         del self
-                    Secure_Connect.existing_sessions.append(session_key)
+                    Secure_Connect.active_sessions.append(session_key)
+                # open connection without bastion
                 else:
                     logging.debug("Class Secure_connect without bastion Started")
                     try:
@@ -141,7 +144,7 @@ class Secure_Connect():
                             self.ssh.close()
                             del self.ssh
                         del self
-                    Secure_Connect.existing_sessions.append(session_key)
+                    Secure_Connect.active_sessions.append(session_key)
                      
 
     def ssh_run(self, cmd):
@@ -158,7 +161,7 @@ class Secure_Connect():
                 self.ssh_bastion.close()
                 del self.ssh_bastion
             del self
-    
+    # after run the caller should call the ssh_del() method so we can check which connections are still valid
     def ssh_del(self):
         with Secure_Connect.global_lock:
             Secure_Connect.manage_sessions(None)
