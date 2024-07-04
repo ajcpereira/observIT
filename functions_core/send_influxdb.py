01f249de2cd000fca804741870201de7e97320a2
ajcpereira <85030333+ajcpereira@users.noreply.github.com>
2024-07-04 15:22:14 +0100

    Update send_influxdb.py

--- functions_core/send_influxdb.py
+++ functions_core/send_influxdb.py
@@ -1,4 +1,3 @@
-
 from influxdb_client.client.write_api import SYNCHRONOUS
 import logging
 import influxdb_client
@@ -9,14 +8,13 @@ def send_influxdb(srv, port, token, org, bucket, data):
     logging.debug("Sending data to InfluxDB server: %s", srv)
 
     try:
-        write_client = influxdb_client.InfluxDBClient(url="http://" + srv + ":" + port, token=token, org=org,timeout=10000, enable_gzip=True)
+        write_client = influxdb_client.InfluxDBClient(url="https://" + srv + ":" + port, token=token, org=org,timeout=10000, enable_gzip=True,verify_ssl=False)
         write_api = write_client.write_api(write_options=SYNCHRONOUS)
         response = write_api.write(bucket=bucket, org=org, record=data,write_precision="s")
         if response:
             logging.error("Received msg from Influxdb during write to server %s with msg %s" % (srv,response)) # response is None if there is no error
-        
+
         write_client.close()
 
     except Exception as msgerror:
         logging.error("Failed to send data to InfluxDB %s - error %s" % (srv, msgerror))
-
