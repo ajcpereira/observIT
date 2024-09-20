f33eb4c701e0b4558c1e95543a08ad8511f14692
ajcpereira <85030333+ajcpereira@users.noreply.github.com>
2024-09-20 13:54:54 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,25 +11,35 @@ change_network() {
     NETMASK=$(dialog --inputbox "Enter new Netmask:" 8 40 3>&1 1>&2 2>&3 3>&-)
     GATEWAY=$(dialog --inputbox "Enter new Default Gateway:" 8 40 3>&1 1>&2 2>&3 3>&-)
     
-    sudo ifconfig eth0 $IP netmask $NETMASK
-    sudo route add default gw $GATEWAY
+    # Update the network configuration file
+    sudo bash -c "cat > /etc/netplan/01-netcfg.yaml <<EOF
+network:
+  version: 2
+  ethernets:
+    eth0:
+      addresses: [$IP/$NETMASK]
+      gateway4: $GATEWAY
+      nameservers:
+        addresses: [8.8.8.8, 8.8.4.4]
+EOF"
+    sudo netplan apply
 }
 
 # Function to change keyboard layout
 change_keyboard() {
-    KEYBOARD=$(dialog --inputbox "Enter new keyboard layout (e.g., us, uk):" 8 40 3>&1 1>&2 2>&3 3>&-)
+    KEYBOARD=$(dialog --menu "Choose a keyboard layout:" 15 50 10 $(localectl list-x11-keymap-layouts | awk '{print NR, $1}') 3>&1 1>&2 2>&3 3>&-)
     sudo loadkeys $KEYBOARD
 }
 
 # Function to change timezone
 change_timezone() {
-    TIMEZONE=$(dialog --inputbox "Enter new timezone (e.g., Europe/Lisbon):" 8 40 3>&1 1>&2 2>&3 3>&-)
+    TIMEZONE=$(dialog --menu "Choose a timezone:" 15 50 10 $(timedatectl list-timezones | awk '{print NR, $1}') 3>&1 1>&2 2>&3 3>&-)
     sudo timedatectl set-timezone $TIMEZONE
 }
 
 # Main menu
 while true; do
-    CHOICE=$(dialog --menu "System Configuration Menu" 15 50 5 \
+    CHOICE=$(dialog --menu "System Configuration Menu" 15 50 6 \
     1 "Update OS" \
     2 "Change Network Settings" \
     3 "Change Keyboard Layout" \
