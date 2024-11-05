69f56ceea855d101a90cbb64b45f17ec9ac5dca1
Alexandre Pereira <ajcpereira@pm.me>
2024-11-05 17:48:09 +0000

    typo in grafana_fun.py

--- functions_core/grafana_fun.py
+++ functions_core/grafana_fun.py
@@ -1730,8 +1730,4 @@ def graph_powerstore_space(system_name, resource_name, metric, y_pos):
 
     line = line + 7
 
-<<<<<<< HEAD
     return line, panels_list
-=======
-    return line, panels_list
->>>>>>> refs/remotes/origin/powerstor_metrics
